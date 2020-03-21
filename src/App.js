877b4dd7ad399cb57637e48d4194cf85f97b7ae6
brainsid <pd-brainsid@alterpost.org>
2020-03-21 18:17:33 +0200

    route to hats

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import './App.css';
+import {Route, Switch} from 'react-router-dom';
 
-import HomePage from './pages/homepage/homepage.component'
+import HomePage from './pages/homepage/homepage.component';
+
+const HatsPage = () => <h1>This is the Hats Page</h1>;
 
 function App() {
   return (
-    <HomePage/>
+    <div>
+      <Route path="/" component={HomePage} exact />
+      <Route path="/shop/hats" component={HatsPage} exact />
+    </div>
   );
 }
 
 export default App;
 
-// 63
+// 67
