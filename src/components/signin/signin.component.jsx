ac509c5075b88692ad06c56f05c45bf1d4839c2e
brainsid <pd-brainsid@alterpost.org>
2020-03-24 13:47:36 +0200

    authentication finished

--- src/components/signin/signin.component.jsx
+++ src/components/signin/signin.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './signin.styles.scss';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
-import {signInWithGoogle} from '../../firebase/firebase.utils';
+import {auth, signInWithGoogle} from '../../firebase/firebase.utils';
 
 class SignIn extends React.Component {
   constructor(props) {
@@ -21,8 +21,13 @@ class SignIn extends React.Component {
 
   handleSubmit = event => {
     event.preventDefault();
-
-    this.setState({email: '', password: ''});
+    const {email, password} = this.state;
+    try {
+      auth.signInWithEmailAndPassword(email, password);
+      this.setState({email: '', password: ''});
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   render = () => (
